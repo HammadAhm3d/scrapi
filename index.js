711d492aa2fa59db1092b087df5672bc6033cf3d
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-02 19:42:19 +0500

    changing

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ app.use('/links', scraperouter);
 
 const PORT= process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == "production") {
+// if (process.env.NODE_ENV == "production") {
     app.use(express.static('/frontend/build'));
-}
+// }
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
