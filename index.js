31f2f710db9681f4565f34d7fa062f1a5b6f8d36
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-02 19:48:34 +0500

    configured before deployment

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express =require('express');
 const bodyParser=require('body-parser');
 const cors =require('cors');
 const scraperouter =require('./routes/links.js');
+const path = require("path");
+
 
 
 
@@ -20,7 +22,7 @@ app.use('/links', scraperouter);
 const PORT= process.env.PORT || 5000;
 
 // if (process.env.NODE_ENV == "production") {
-    app.use(express.static('/frontend/build'));
+    app.use(express.static(path.join(__dirname, 'frontend', "build")));
 // }
 
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
