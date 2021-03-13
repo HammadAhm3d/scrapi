2256fe8eb30e00952308f7e80e450e3d562bc008
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-13 22:46:18 +0500

    google scholar

--- controller/google_scholar_links.py
+++ controller/google_scholar_links.py
@@ -27,12 +27,12 @@ def get_links(user_input):
 	# Test
 	chrome_options = webdriver.ChromeOptions()
 	
-	# chrome_options.add_argument("--headless")
-	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument("--disable-dev-shm-usage")
-	chrome_options.add_argument("--user-data-dir=chrome-data")
-	chrome_options.add_argument("user-data-dir=chrome-data") 
+	# chrome_options.add_argument("--user-data-dir=chrome-data")
+	# chrome_options.add_argument("user-data-dir=chrome-data") 
 	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
