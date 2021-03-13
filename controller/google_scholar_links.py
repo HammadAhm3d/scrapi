23128d28a9efafa8f63adb6eeabea3d680828bcd
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-13 21:58:52 +0500

    working locally

--- controller/google_scholar_links.py
+++ controller/google_scholar_links.py
@@ -31,7 +31,6 @@ def get_links(user_input):
 	chrome_options.add_argument('--disable-gpu')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument("--disable-dev-shm-usage")
-	chrome_options.binary_location = GOOGLE_CHROME_PATH
 	chrome_options.add_argument("--user-data-dir=chrome-data")
 	chrome_options.add_argument("user-data-dir=chrome-data") 
 	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
