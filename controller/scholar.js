6e46296de62038c8c0a95d9adf031c2f6fea5df4
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-18 17:28:20 +0500

    works local

--- controller/scholar.js
+++ controller/scholar.js
@@ -1,4 +1,4 @@
-// require('chromedriver');
+require('chromedriver');
 const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
@@ -12,10 +12,10 @@ exports.getScholarLinks = async(req, res) => {
 
     options = new chrome.Options();
     //uncomment for heroku
-    serviceBuilder = new chrome.ServiceBuilder(process.env.CHROMEDRIVER_PATH);
+    // serviceBuilder = new chrome.ServiceBuilder(process.env.CHROMEDRIVER_PATH);
     //comment for heroku
-    // serviceBuilder = new chrome.ServiceBuilder();
-    options.setBinary(process.env.GOOGLE_CHROME_BIN);
+    serviceBuilder = new chrome.ServiceBuilder();
+    // options.setBinary(process.env.GOOGLE_CHROME_BIN);
 
     options.addArguments("--headless");
     options.addArguments("--disable-gpu");
