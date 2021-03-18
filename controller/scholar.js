64cd02938a5f202f7b00f72af33e17eee50f5af3
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-18 16:31:43 +0500

    using selenium in node now

---
+++ controller/scholar.js
@@ -0,0 +1,66 @@
+require('chromedriver');
+const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+
+
+exports.getScholarLinks = async(req, res) => {
+    console.log(req.body);
+    const url = `https://scholar.google.com/`;
+    var options = null;
+    var serviceBuilder = null;
+
+    options = new chrome.Options();
+    //uncomment for heroku
+    //serviceBuilder = new firefox.ServiceBuilder(process.env.GECKODRIVER_PATH);
+    //comment for heroku
+    // serviceBuilder = new chrome.ServiceBuilder();
+    // options.setBinary(process.env.FIREFOX_BIN || "C:\\firefoxdriver.exe");
+    // options.setBinary(process.env.GOOGLE_CHROME_BIN || "/controller/chromedriver.exe");
+
+    options.addArguments("--headless");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--no-sandbox");
+    console.log(`inside getScholarLinks()`);
+    let driver = null;
+    var scholarLinks = [];
+    try{
+        driver = await new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(options)
+            .build();
+        await driver.get(`${url}scholar?q=${req.body.inputt}`);
+        let page_no = 1;
+	    let pages_limit = 4;
+        while(page_no<pages_limit){
+            await driver.wait(until.elementsLocated(By.xpath(`//div[@class='gs_or_ggsm']`)), 12000);
+            let divs = await driver.findElements(By.xpath(`//div[@class='gs_or_ggsm']`))
+            console.log(divs.length);
+            for(let i = 0; i <divs.length; i++) {
+                var arr = await divs[i].findElements(By.tagName('a'));
+                console.log(arr.length);
+                for(let j = 0; j < arr.length; j++) {
+
+                    try{
+
+                        let att = await arr[j].getAttribute('href');
+                        if(att != null || att != 'null' || att != undefined)
+                            scholarLinks.push(att)
+
+                    }catch(e) {
+                        console.log(e.message);
+                    }
+                }
+            }
+            page_no=page_no+1
+            // await driver.findElement(By.xpath("//span[@class='gs_ico gs_ico_nav_next']")).click();
+        }
+        console.log(scholarLinks);
+        res.json({links: scholarLinks, success: true});
+    }catch(e){
+        console.log(e.message);
+        res.json({links: e.message, success: false});
+
+    }
+
+}
--- routes/links.js
+++ routes/links.js
@@ -1,7 +1,11 @@
 const express= require( "express");
 const {scrape}=require("../controller/getlinks.js");
+const{getScholarLinks} = require('../controller/scholar');
 const router=express.Router()
 
-router.post('/',scrape)
+// router.post('/',scrape)
+
+router.post('/',getScholarLinks)
+
 
 module.exports=router;
