b04195efb21562bc2fb502412a1b59150f9c5b57
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2021-03-18 19:40:03 +0500

    push your code to heroku

--- controller/scholar.js
+++ controller/scholar.js
@@ -1,9 +1,9 @@
-const {Browser, Builder, By, until } = require('selenium-webdriver');
-const {Options, ServiceBuilder} = require('selenium-webdriver/chrome');
+const { Browser, Builder, By, until } = require('selenium-webdriver');
+const { Options, ServiceBuilder } = require('selenium-webdriver/chrome');
 
 
 
-exports.getScholarLinks = async(req, res) => {
+exports.getScholarLinks = async (req, res) => {
     console.log(req.body);
     const url = `https://scholar.google.com/`;
     var options = null;
@@ -16,59 +16,61 @@ exports.getScholarLinks = async(req, res) => {
     options.addArguments("--no-sandbox");
 
     //for running locally use following
-    require('chromedriver');
-    // require('geckodriver');
+    //require('chromedriver');
     serviceBuilder = new ServiceBuilder();
-    // options.setBinary(process.env.FIREFOX_BIN || "C:\\firefoxdriver.exe");
 
     //for running on heroku use following
-    // options.setChromeBinaryPath(process.env.GOOGLE_CHROME_BIN);
-    // serviceBuilder = new ServiceBuilder(process.env.CHROMEDRIVER_PATH);
+    options.setChromeBinaryPath(process.env.GOOGLE_CHROME_BIN);
+    //serviceBuilder = new ServiceBuilder(process.env.CHROMEDRIVER_PATH);
 
 
-
-    
     console.log(`inside getScholarLinks()`);
     let driver = null;
     var scholarLinks = [];
-    try{
+    try {
         driver = await new Builder()
-            .forBrowser(Browser.CHROME)
+            .forBrowser('chrome')
             .setChromeOptions(options)
             .setChromeService(serviceBuilder)
             .build();
         await driver.get(`${url}scholar?q=${req.body.inputt}`);
         let page_no = 1;
-	    let pages_limit = 4;
-        while(page_no<pages_limit){
-            await driver.wait(until.elementsLocated(By.xpath(`//div[@class='gs_or_ggsm']`)), 12000);
+        let pages_limit = 4;
+        while (page_no < pages_limit) {
+            await driver.wait(until.elementsLocated(By.xpath(`//div[@class='gs_or_ggsm']`)), 20000);
             let divs = await driver.findElements(By.xpath(`//div[@class='gs_or_ggsm']`))
             console.log(divs.length);
-            for(let i = 0; i <divs.length; i++) {
+            for (let i = 0; i < divs.length; i++) {
                 var arr = await divs[i].findElements(By.tagName('a'));
                 console.log(arr.length);
-                for(let j = 0; j < arr.length; j++) {
+                for (let j = 0; j < arr.length; j++) {
 
-                    try{
+                    try {
 
                         let att = await arr[j].getAttribute('href');
-                        if(att != null || att != 'null' || att != undefined)
+                        if (att != null || att != 'null' || att != undefined)
                             scholarLinks.push(att)
 
-                    }catch(e) {
+                    } catch (e) {
                         console.log(e.message);
                     }
                 }
             }
-            page_no=page_no+1
+            page_no = page_no + 1
             // await driver.findElement(By.xpath("//span[@class='gs_ico gs_ico_nav_next']")).click();
         }
         console.log(scholarLinks);
-        res.json({links: scholarLinks, success: true});
-    }catch(e){
+        res.json({ links: scholarLinks, success: true });
+    } catch (e) {
         console.log(e.message);
-        res.json({links: e.message, success: false});
+        res.json({ links: e.message, success: false });
 
+    } finally {
+        try {
+            driver.close();
+        } catch (e) {
+
+        }
     }
 
 }
